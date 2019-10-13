59e1a8d330e6980033000466587a34d7e99fbee3
Iryu <leonel.yoyo@gmail.com>
2019-10-13 17:50:40 +0200

    C:/Program Files/Git/

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         Get direct links of OpenLoad
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match       https://openload.pw/*
+// @match       https://oload.monster/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
 var button = document.createElement('div');
 button.innerHTML = `<button id="getLink" type="button" style="position: absolute; top: 0px;">Find Link : Press me after: 1. Setting player to OpenLoad 2. Clicking the play button</button>`;
 var sidebar = document.getElementById('mediaspace_wrapper');
@@ -9,11 +22,8 @@ var clickAction = function () {
   var url = location.slice(0, index + 2)
   var l = $('video').attr('src');
   var link = url + l
-  if (l !== undefined) {
-    window.open(link, '_self');
-  } else {
-    window.alert('Something Went Wrong, Make sure you follow instructions');
-  }
+  window.open(link, '_self');
 };
 
 button.addEventListener('click', clickAction, false);
+})();
